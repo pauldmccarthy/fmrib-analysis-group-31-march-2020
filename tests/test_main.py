788275518f9940dfdf8842eabc7c24eb95967820
Paul McCarthy <pauldmccarthy@gmail.com>
2020-02-12 18:31:52 +0000

    TEST: use tempdir

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python
 
+
+import os
+import shutil
+import tempfile
+import contextlib
+
 import numpy as np
 
 import analysis.main as main
@@ -9,6 +15,18 @@ test_model = np.array([[0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1],
 test_data  = (test_model[:, 0] * 5) + (test_model[:, 1] * 20)
 
 
+@contextlib.contextmanager
+def tempdir():
+    testdir = tempfile.mkdtemp()
+    prevdir = os.getcwd()
+    try:
+        os.chdir(testdir)
+        yield testdir
+    finally:
+        os.chdir(prevdir)
+        shutil.rmtree(testdir)
+
+
 def test_ols():
     fit, err = main.ols(test_data, test_model)
     assert np.isclose(fit, [5, 20]).all()
@@ -16,13 +34,14 @@ def test_ols():
 
 
 def test_main():
-    np.savetxt('data.txt', test_data)
-    np.savetxt('model.txt', test_model)
+    with tempdir():
+        np.savetxt('data.txt', test_data)
+        np.savetxt('model.txt', test_model)
 
-    main.main('data.txt', 'model.txt', 'pes.txt', 'residuals.txt')
+        main.main('data.txt', 'model.txt', 'pes.txt', 'residuals.txt')
 
-    fit = np.loadtxt('pes.txt')
-    err = np.loadtxt('residuals.txt')
+        fit = np.loadtxt('pes.txt')
+        err = np.loadtxt('residuals.txt')
 
-    assert np.isclose(fit, [5, 20]).all()
-    assert np.isclose(err, 0)      .all()
+        assert np.isclose(fit, [5, 20]).all()
+        assert np.isclose(err, 0)      .all()
